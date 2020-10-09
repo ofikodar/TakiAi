42feced08fca4e427d18707a2fc4a4d412603f02
ofikodar <ofikodar@gmail.com>
2020-10-09 20:08:18 +0300

    add pull and shuffle to deck

--- src/Game/deck.py
+++ src/Game/deck.py
@@ -1,16 +1,27 @@
+import random
+
 from src.Game.cards import RegularCard, StrongCard, SuperCard
 
 CARD_COLORS = ['red', 'yellow', 'blue', 'green']
-STRONG_CARDS_NAMES = ['stop', 'taki', 'add', 'takeTwo']
-SUPER_CARDS_NAMES = ['change', 'super', 'king']
+STRONG_CARDS_NAMES = ['change direction', 'stop', 'taki', 'add one', 'take two']
+SUPER_CARDS_NAMES = ['change color', 'super', 'king']
 
 
 class Deck:
     def __init__(self):
         self.deck = []
+        self._create_cards()
+
+    def pull(self):
+        if len(self.deck) == 0:
+            self._create_cards()
+        del self.deck[0]
+
+    def _create_cards(self):
         self._create_regulars()
         self._create_strong()
         self._create_super()
+        random.shuffle(self.deck)
 
     def _create_regulars(self):
         regulars = [RegularCard(number, color) for color in CARD_COLORS for number in range(1, 10)] * 2
@@ -27,4 +38,6 @@ class Deck:
 
 
 if __name__ == '__main__':
-    Deck()
+    deck = Deck()
+    for i in range(130):
+        deck.pull()
