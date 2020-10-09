efd8f67c99d56439ac9bb8bb46e1c14cb0251dc2
ofikodar <ofikodar@gmail.com>
2020-10-09 19:19:18 +0300

    add cards classes

---
+++ src/Game/cards.py
@@ -0,0 +1,16 @@
+class RegularCard:
+    def __init__(self, number, color):
+        self.number = number
+        self.color = color
+
+
+class StrongCard:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+class SuperCard:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
